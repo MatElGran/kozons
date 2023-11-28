d1e5fbd9cfc7e69deb26205d57b97158db6a6e4d
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2023-11-28 19:55:04 +0100

    Add base plugin to handle subfolder deployment

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy')
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('bundle.css')
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
+
   return {
     dir: { input: 'content', includes: '../_includes' }
   }
