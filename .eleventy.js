ea287f73ba9569ddbd028cc8eec0d07b30c10efc
Mathieu Le Tiec <mathieu.letiec+github@gmail.com>
2023-12-06 19:31:29 +0100

    First draft at landing with hero

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { EleventyHtmlBasePlugin } = require('@11ty/eleventy')
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('assets/images')
   eleventyConfig.addPassthroughCopy('bundle.css')
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
 
